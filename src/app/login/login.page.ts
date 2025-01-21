797bbd522b5f5d99db8670f1b73e49c7ecf14742
Angel <20233l001142@utcv.edu.mx>
2025-01-21 10:24:21 -0600

    feat: crear un tema nuevo

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {NavController} from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import {  LoadingController } from '@ionic/angular/standalone';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -13,30 +15,47 @@ export class LoginPage implements OnInit {
 email: string = '';
 password: string = '';
 
-  constructor(private navController: NavController,private toastController: ToastController) { }
+  constructor(private navController: NavController,private toastController: ToastController, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
   }
 
-  async presentToast() {
+  async presentToast(message: string, color:string) {
     const toast = await this.toastController.create({
-      message: 'Credenciales incorrectas',
+      message: message,
       duration: 1500,
       position: 'bottom',
-      color: 'danger',
+      color: color,
     });
 
     await toast.present();
   }
 
-login(){
-  if(this.email === 'correo@gmail.com' && this.password === 'pass' && this.user === 'riv'){
-    this.navController.navigateRoot('/casa');
-  
-  }else{
-    this.presentToast();
+  async login(){
+    const localEmail = localStorage.getItem('userEmail');
+    const localPassword = localStorage.getItem('userPassword');
+    const localUser = localStorage.getItem('userName');
+   
+    console.log(localUser,' ' , localEmail , ' ', localPassword);
+    if(this.email === localEmail && this.password === localPassword && this.user === localUser){
+    await this.showLoading();
+      this.presentToast('Inicio exitoso', 'primary')
+      this.navController.navigateRoot('/casa');
+    
+    }else{
+      this.presentToast('Credenciales invalidas', 'warning');
+    }
+   
   }
-  
+
+async showLoading() {
+  const loading = await this.loadingCtrl.create({
+    message: 'Cargando...',
+    duration: 1000,
+  });
+
+  loading.present();
+  await loading.onDidDismiss();
 }
 
 }
--- src/app/registrar/registrar.page.ts
+++ src/app/registrar/registrar.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
+import {NgForm} from '@angular/forms';
+
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.page.html',
@@ -12,29 +14,51 @@ export class RegistrarPage implements OnInit {
   user: string = '';
   email: string = '';
   password: string = '';
+  confirmPassword: string ='';
 
   constructor(private router: Router,private toastController: ToastController) { }
 
   ngOnInit() {
   }
 
-registrar (){
-  localStorage.setItem('userEmail', this.email);
-  localStorage.setItem('userPassword', this.password);
-  localStorage.setItem('userName' , this.user);
+registrar (form: NgForm){
+console.log(form.value);
+console.log(form.valid)
 
-  async presentToast() {
-    const toast = await this.toastController.create({
-      message: 'Credenciales incorrectas',
-      duration: 1500,
-      position: 'bottom',
-      color: 'danger',
-    });
+  if(form.valid){
+    if (form.value.password!=form.value.confirmPassword){
 
-    await toast.present();
+      this.presentToast('Contraseñas diferentes', 'warning');
+     
+    }else{
+      
+      localStorage.setItem('userEmail', this.email);
+      localStorage.setItem('userPassword', this.password);
+      localStorage.setItem('userName' , this.user);  
+      this.presentToast('Registro exitoso', 'primary');
+      this.router.navigate(['/login']); 
+    
+    }
+    
+  }else{
+    this.presentToast('Rellena todos los campos','warning');        
   }
+
+
+ 
+
   
-  this.router.navigate(['/login']);
+}
+
+async presentToast(message: string, color: string,) {
+  const toast = await this.toastController.create({
+    message: message,
+    duration: 1500,
+    position: 'bottom',
+    color: color,
+  });
+
+  await toast.present();
 }
 
 }
